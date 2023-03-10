16800922832b9adcaa38237a5781bc824513e8a2
DaniJFerr <daniel_jferreira@hotmail.com>
2023-03-10 08:40:41 +0000

    add github repositoy link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year} <a href="https://github.com/DaniJFerreira/keeper-web-app"><i class="fa-brands fa-github"></i></a></p>
     </footer>
   );
 }
